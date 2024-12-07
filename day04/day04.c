faf3f1555611bde16a33007d605a011c5ee7e8ea
Kuzey Kara <kuzeykara07@gmail.com>
2024-12-07 02:25:44 +0100

    day 4 complete

--- day04/day04.c
+++ day04/day04.c
@@ -122,6 +122,77 @@ int find_word(char ws[][MAX_LINE_LENGTH], int lineC, char pattern[], int pattern
     return word_count;
 }
 
+int find_X_MAS(char ws[][MAX_LINE_LENGTH], int lineC)
+{
+    int reverse_flag;
+    int X_count = 0;
+    
+    for (int y=0; y<lineC; y++) {
+        for (int x=0; x<lineC; x++) {
+            // look for A, check topleft bottom right, check topright bottomleft
+            if (ws[y][x] == 'A') {
+                //top left
+                reverse_flag = 0;
+                if (y-1 >= 0 && x-1 >= 0) {
+                    if (ws[y-1][x-1] == 'S') {
+                    reverse_flag = 1;
+                    } else if (ws[y-1][x-1] != 'M') {
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+
+                //bottom right
+                if (y+1 < lineC && x+1 < lineC) {
+                    if (reverse_flag) {
+                        if (ws[y+1][x+1] != 'M') {
+                            continue;
+                        }
+                    } else {
+                        if (ws[y+1][x+1] != 'S') {
+                            continue;
+                        }
+                    }
+                } else {
+                    continue;
+                }
+
+                //top right
+                reverse_flag = 0;
+                if (y-1 >= 0 && x+1 < lineC) {
+                    if (ws[y-1][x+1] == 'S') {
+                        reverse_flag = 1;
+                    } else if (ws[y-1][x+1] != 'M') {
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+                
+                //bottom left
+                if (y+1 < lineC && x-1 >= 0) {
+                    if (reverse_flag) {
+                        if (ws[y+1][x-1] != 'M') {
+                            continue;
+                        }
+                    } else {
+                        if (ws[y+1][x-1] != 'S') {
+                            continue;
+                        }
+                    }
+                } else {
+                    continue;
+                }
+
+                X_count++;
+            }
+        }
+    }
+
+    return X_count;
+}
+
 int part1(char* filename)
 {
     FILE *fp;
@@ -156,9 +227,20 @@ int part2(char* filename)
         exit(1);
     }
 
+    char line[MAX_LINE_LENGTH];
+    char wordSearch[MAX_LINE_LENGTH][MAX_LINE_LENGTH];
+    int lineC = 0;
+
+    while (fgets(line, sizeof(line), fp)) {
+        strcpy(wordSearch[lineC], line);
+        lineC++;
+    }
+
+    // print_word_search(wordSearch, lineC);
+
     fclose(fp);
 
-    return 0;
+    return find_X_MAS(wordSearch, lineC);
 }
 
 int main(int argc, char** argv)
